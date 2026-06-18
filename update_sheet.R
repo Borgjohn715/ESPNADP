b801bbc86f7ed2b8a36fc555bffb18545cd5429e
Borgjohn715 <149623646+Borgjohn715@users.noreply.github.com>
2026-06-18 13:07:55 -0400

    Update update_sheet.R

--- update_sheet.R
+++ update_sheet.R
@@ -11,10 +11,10 @@ gs4_auth(path = "service-account.json")
 player_info <- ffanalytics::espn_draft()
 
 # 2. Specify the Google Sheet ID
-sheet_id <- "1htXgq2ti-gE5XdOXif1ADr6IIaTEPWkTVVMIaD-d-00"
+sheet_id <- "1Ap4NUbXeXl17W5-8jQj-smPQ1DYOUiRNlMyc3KWKbgA"
 
 # 3. Create a name for the new sheet/tab
-new_tab_name <- format(Sys.Date(), "%Y-%m-%d")
+new_tab_name <- format(Sys.Date(), "%m-%d")
 
 # 4. Write the data to the new sheet
 sheet_write(player_info, ss = sheet_id, sheet = new_tab_name)
